1a4b0312abbc93f2da26e363c2be388beecf0f21
Szymon <sz.oleskiewicz@gmail.com>
2026-01-03 15:56:47 +0100

    Updated main.py with first example usage of new environment

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from typing import List
+from simulation import Simulation
+
+def test_sim():
+    sim = Simulation(32, 32)
+    sources = {
+        'energy': 1,
+        'co2': 2,
+        'n2': 2,
+    }
+    sim.setup_environment(sources)
+
+    sim.run(100, 10)
 
 
-def test_sim() -> List:
-    results = []
 
-    return results
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     test_sim()
